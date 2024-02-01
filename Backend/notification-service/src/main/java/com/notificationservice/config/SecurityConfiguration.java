e14f1e111d1af9ea8ad89b7a76c4d52a43dd5787
Misha Khapalnov <michkahap@gmail.com>
2024-02-01 15:55:22 +0200

    Some change

--- Backend/notification-service/src/main/java/com/notificationservice/config/SecurityConfiguration.java
+++ Backend/notification-service/src/main/java/com/notificationservice/config/SecurityConfiguration.java
@@ -23,6 +23,7 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/massage/**").permitAll()
                     .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
